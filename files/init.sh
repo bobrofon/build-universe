62cb4216a9afe073be117f50f663b6842d374bc1
Бобренок Сергей Иванович <s.bobrenok@2gis.ru>
2016-09-02 11:57:01 +0700

    Add running with host user uid/gid

--- files/init.sh
+++ files/init.sh
@@ -1,5 +1,18 @@
 #! /bin/sh
 
+set -eu
+
+UID=$1
+shift
+GID=$1
+shift
+
+USER=hostuser
+GROUP=hostgroup
+
+groupadd --gid ${GID} ${GROUP}
+useradd --no-user-group --no-create-home --uid=${UID} --gid=${GID} ${USER}
+
 mount ${TOOLCHAIN_BASE_DIR}/${TOOLCHAIN_IMAGE} ${TOOLCHAIN_PATH}
 for FILE in ${TOOLCHAIN_PATH}/bin/${TARGET}-*
 do
@@ -11,5 +24,5 @@ ln -s ${LD_LIBRARY_PATH}/ld-linux.so.3 /lib/ld-linux.so.3
 
 export PATH=${PATH}:${TOOLCHAIN_PATH}/bin
 
-cmd="${@:-bash}"
-eval "$cmd"
+CMD="${@:-bash}"
+su ${USER} -c "${CMD}"
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 CONTAINER_NAME="arm_universe"
-docker run -v ${PWD}:/src --rm --privileged -it ${CONTAINER_NAME} "${@}"
+docker run -v ${PWD}:/src --rm --privileged -it ${CONTAINER_NAME} $(id -u) $(id -g) "${@}"
